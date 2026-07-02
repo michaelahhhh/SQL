4f817a432e869447854c0f5a37276cc163728669
Esteban Ibañez <139912376+mancomanXD777@users.noreply.github.com>
2026-07-02 16:12:30 -0400

    Refactor menus to use while loops for navigation

--- comerciotech/main.py
+++ comerciotech/main.py
@@ -1,85 +1,105 @@
 from cliente import *
 from productos import *
 from pedidos import *
-while True:
 
 
-    def menu_principal():
-        print("<|||menu principal de comerciotech|||>")
-        print("1. menu de productos")
-        print("2. menu de clientes")
-        print("3. menu de pedidos")
-        print("4. Salir")
-
-
-    input_usuario = input("Ingrese una opción: ")
-    if input_usuario == "1":
-        menu_productos()
-    elif input_usuario == "2":
-        menu_clientes()
-    elif input_usuario == "3":
-        menu_pedidos()
-    elif input_usuario == "4":
-            print("Saliendo del programa...")
-            break 
-
-
-    def menu_productos():
-        print("<|||menu de productos|||>")
+def menu_productos():
+    while True:
+        print("\n<||| MENÚ PRODUCTOS |||>")
         print("1. Agregar producto")
         print("2. Mostrar productos")
         print("3. Modificar producto")
         print("4. Eliminar producto")
-        print("5. Volver al menú principal")
+        print("5. Volver")
 
+        opcion = input("Ingrese opción: ")
 
-        if input_usuario == "1":
+        if opcion == "1":
             agregar_producto()
-        elif input_usuario == "2":
+        elif opcion == "2":
             mostrar_productos()
-        elif input_usuario == "3":
+        elif opcion == "3":
             modificar_producto()
-        elif input_usuario == "4":
+        elif opcion == "4":
             eliminar_producto()
-        elif input_usuario == "5":
-            menu_principal()
+        elif opcion == "5":
+            break
+        else:
+            print("Opción inválida")
 
 
-    def menu_clientes():
-        print("<|||menu de clientes|||>")
+def menu_clientes():
+    while True:
+        print("\n<||| MENÚ CLIENTES |||>")
         print("1. Agregar cliente")
         print("2. Mostrar clientes")
         print("3. Modificar cliente")
         print("4. Eliminar cliente")
-        print("5. Volver al menú principal")
+        print("5. Volver")
 
+        opcion = input("Ingrese opción: ")
 
-        if input_usuario == "1":
+        if opcion == "1":
             agregar_cliente()
-        elif input_usuario == "2":
+        elif opcion == "2":
             mostrar_clientes()
-        elif input_usuario == "3":
+        elif opcion == "3":
             modificar_cliente()
-        elif input_usuario == "4":
+        elif opcion == "4":
             eliminar_cliente()
-        elif input_usuario == "5":
-            menu_principal()
+        elif opcion == "5":
+            break
+        else:
+            print("Opción inválida")
 
-    def menu_pedidos():
-        print("<|||menu de pedidos|||>")
+
+def menu_pedidos():
+    while True:
+        print("\n<||| MENÚ PEDIDOS |||>")
         print("1. Agregar pedido")
         print("2. Mostrar pedidos")
         print("3. Modificar pedido")
         print("4. Eliminar pedido")
-        print("5. Volver al menú principal")
+        print("5. Volver")
 
-        if input_usuario == "1":
+        opcion = input("Ingrese opción: ")
+
+        if opcion == "1":
             agregar_pedido()
-        elif input_usuario == "2":
+        elif opcion == "2":
             mostrar_pedidos()
-        elif input_usuario == "3":  
+        elif opcion == "3":
             modificar_pedido()
-        elif input_usuario == "4":
+        elif opcion == "4":
             eliminar_pedido()
-        elif input_usuario == "5":
-            menu_principal()
+        elif opcion == "5":
+            break
+        else:
+            print("Opción inválida")
+
+
+def menu_principal():
+    while True:
+        print("\n<||| MENÚ PRINCIPAL COMERCIOTECH |||>")
+        print("1. Menú productos")
+        print("2. Menú clientes")
+        print("3. Menú pedidos")
+        print("4. Salir")
+
+        opcion = input("Ingrese una opción: ")
+
+        if opcion == "1":
+            menu_productos()
+        elif opcion == "2":
+            menu_clientes()
+        elif opcion == "3":
+            menu_pedidos()
+        elif opcion == "4":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción inválida")
+
+
+
+menu_principal()
